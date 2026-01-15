507aaa23fea78da4071c0eb5a7b769b162cfc89c
Nehul Gupta <ng5551@srmist.edu.in>
2026-01-15 11:39:37 +0530

    Remove objectFit style from Image component
    Removed objectFit style from Image component.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -178,7 +178,7 @@ export default function Home() {
                   src="/ChatGPT Image Jan 14, 2026, 12_25_16 AM.png"
                   alt=""
                   fill
-                  style={{ objectFit: "contain", borderRadius: "10px" }}
+                  style={{ borderRadius: "10px" }}
                   priority
                 />
               </div>
